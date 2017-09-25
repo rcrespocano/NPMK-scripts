8c40de8e00155a62e5ea8a9de904a7b61fe03661
Rubén Crespo-Cano <rcrespocano@gmail.com>
2017-09-25 11:28:00 +0200

    New module to manage I/O operations

---
+++ npmkscripts/io.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Helper utilities for io."""
+
+import cv2
+from os import listdir
+from os.path import isfile, join
+
+
+def build_video(_output_folder, _output_video_ext, _file_pattern, _file_ext, _fps):
+    """
+    Function to build the output video with the processed frames.
+    """
+    _codec_fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    _image_list = [f for f in listdir(_output_folder) if isfile(join(_output_folder, f))
+                   and f.find(_file_pattern) >= 0 and f.find(_file_ext) > 0]
+    _image_list.sort()
+
+    # Video writer
+    _tmp_image = cv2.imread(_output_folder + _image_list[0])
+    _shape = _tmp_image.shape
+    _writer = cv2.VideoWriter(_output_folder + _file_pattern + _output_video_ext, _codec_fourcc, _fps, (_shape[1],
+                                                                                                        _shape[0]))
+
+    for _file_str in _image_list:
+        _frame = cv2.imread(_output_folder + _file_str)
+        _writer.write(_frame)
+
+    _writer.release()
