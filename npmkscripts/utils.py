864f7c8f8d6a542d3485c44ce93286c8e8a2e622
Rubén Crespo-Cano <rcrespocano@gmail.com>
2017-09-26 13:23:02 +0200

    New method to obtain current datetime formated

--- npmkscripts/utils.py
+++ npmkscripts/utils.py
@@ -2,8 +2,13 @@
 """Utilities module."""
 
 import sys
+import datetime
 
 
 def check_python_version():
     if sys.version_info[0] < 3:
         raise TypeError('This program must be executed with Python 3')
+
+
+def get_datetime():
+    return datetime.datetime.now().strftime('%Y%m%d%H%M%S')
